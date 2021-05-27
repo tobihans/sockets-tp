981731bd41a361d44bff7313f539cfea86c20d3e
olabijed@gmail.com <olabijed@gmail.com>
2021-05-27 13:31:19 +0100

    Mise à jour C++ (Qt) et C#

--- C++/SocketClient/main.cpp
+++ C++/SocketClient/main.cpp
@@ -1,7 +1,5 @@
 #include <QCoreApplication>
-#include <QObject>
 #include <QString>
-#include <iostream>
 #include <QTextStream>
 #include <QHostAddress>
 #include <QUdpSocket>
@@ -14,22 +12,23 @@ int main(int argc, char *argv[])
     auto socket = new QUdpSocket();
     // Connexion en local sur le port défini
     socket->connectToHost(QHostAddress::LocalHostIPv6,PORT);
+    // On attend pendant 45s une conenxion entrante
     if(socket->waitForConnected(45000))
     {
         while (true) // Boucle principale du progamme
         {
             QTextStream out{stdout};
             out << "> ";
-            // On lit a travers l'entrée standard pour rnvoyer notre message
+            // On lit a travers l'entrée standard pour envoyer notre message
             QTextStream in{stdin};
-            QString msg = in.readLine();
+            const QString msg = in.readLine();
             socket->write(msg.toLatin1());
             if(socket->waitForBytesWritten(1000))
             {
                 qDebug() << "Message envoyé";
                 if(socket->waitForReadyRead())
                 {
-                    QString response{socket->readAll()};
+                    const QString response{socket->readAll()};
                     // Lorsqu'on recoit la chaine de fin de communication, on quitte la boucle
                     if(response.indexOf(EOC) > -1) break;
                     qDebug() << "Response : " << response;
